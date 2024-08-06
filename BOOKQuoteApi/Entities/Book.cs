78e40a77fa76d03fa1c8d7ddb3605e863cc17e85
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-08-07 00:38:44 +0200

    Add project files.

---
+++ BOOKQuoteApi/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BOOKQuoteApi.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
---
+++ BOOKQuoteApi/Entities/Quote.cs
@@ -0,0 +1,9 @@
+﻿namespace BOOKQuoteApi.Entities
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteText { get; set; }
+        public string Author { get; set; }
+    }
+}
